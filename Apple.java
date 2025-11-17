bdb8121c3b2176fcbc0e19a85482714ffcb4ced5
kolbyng <440021056@gapps.yrdsb.ca>
2025-11-17 14:10:13 -0500

    apple creation script

---
+++ Apple.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Apple actor
+ * Kolby Ng
+ * 17/11/2025
+ */
+public class Apple extends Actor
+{
+    public void act()
+    {
+        setLocation(getX(), getY() + 1);
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -5,9 +5,17 @@ import greenfoot.*;
  * Date: 17/11/2025
  */
 public class MyWorld extends World {
-    public MyWorld() {
+    public MyWorld() 
+    {
         super(600, 400, 1, false);
         Elephant e = new Elephant();
         addObject(e, 300, 200);
+        createApple();
+    }
+    
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        addObject(apple, Greenfoot.getRandomNumber(600), 0);
     }
 }
