62fcbc408bb61c11525dd1321d910c4577af8eb2
kolbyng <kolbyng@gmail.com>
2025-11-18 14:44:57 -0500

    added score label

--- MyWorld.java
+++ MyWorld.java
@@ -5,17 +5,27 @@ import greenfoot.*;
  * Date: 17/11/2025
  */
 public class MyWorld extends World {
+    private int score = 0;
+    private Label scoreLabel;
     public MyWorld() 
     {
         super(600, 400, 1, false);
         Elephant e = new Elephant();
         addObject(e, 300, 200);
         createApple();
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
     }
     
     public void createApple()
     {
         Apple apple = new Apple();
         addObject(apple, Greenfoot.getRandomNumber(600), 0);
+    }
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
     }
 }
